70397f5d653fe71274b089b8fe55a97e58fdf56a
Phetsodonald <phetso@DIXON-DNLP1501.>
2026-04-30 11:50:13 +0200

    added a model for users

--- core/settings.py
+++ core/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'bookings',
     'talent',
-    'users'
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+AUTH_USER_MODEL = 'users.User'
--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    ROLE_CHOICE = (
+        ('organizer', 'organizer'),
+        ('talent', 'talent'),
+        ('admin', 'admin'),
+    )
+
+    role = models.CharField(max_length=20, choices=ROLE_CHOICE)
+    phone = models.CharField(max_length=20, blank=True, null=True)
