ae7a122bae938a8812ef9fe0348abca12a20efdb
Phetsodonald <phetso@DIXON-DNLP1501.>
2026-04-30 16:12:17 +0200

    auto genarated a slug

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Booking, Message, Review
 # Register your models here.
+admin.site.register(Booking)
+admin.site.register(Message)
+admin.site.register(Review)
--- talent/models.py
+++ talent/models.py
@@ -1,10 +1,26 @@
 from django.db import models
 from users.models import User
+from django.utils.text import slugify
+
 
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            base_slug = slugify(self.stage_name)
+            slug = base_slug
+            counter = 1
+
+            while TalentProfile.objects.filter(slug=slug).exists():
+                slug = f"{base_slug}-{counter}"
+                counter += 1
+
+            self.slug = slug
+
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
