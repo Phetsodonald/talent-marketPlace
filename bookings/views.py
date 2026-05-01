efaef5f4f6a3ca552d0bb30ee95f7d5f5b2905f4
Phetsodonald <phetso@DIXON-DNLP1501.>
2026-05-01 16:45:53 +0200

    added a serializer for Booking app

--- bookings/views.py
+++ bookings/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Booking
+from .serializers import BookingSerializer
 
 # Create your views here.
+@api_view(['POST'])
+def create_booking(request):
+    serializer = BookingSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=201)
+    
+    return Response(serializer.errors, status=400)
